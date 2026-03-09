ce93c44cb75ba013ea158d6b7bc15ccac4a8ced0
Carlos Racobaldo Montes <carloshracobaldo@gmail.com>
2026-03-09 19:01:57 -0300

    Implementando método deleteById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,36 +13,46 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("=== TESTE 1 Find By ID ====");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("=== TESTE 2 Find By Department ====");
+		System.out.println("\n === TESTE 2 Find By Department ====");
 		Department department = new Department  (2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TESTE 3 Find All ====");
+		System.out.println("\n === TESTE 3 Find All ====");
 		
 		list = sellerDao.findAll();
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TESTE 4 Seller Insert ====");
+		System.out.println("\n === TESTE 4 Seller Insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("=== TESTE 5 Seller Update ====");
+		System.out.println("\n === TESTE 5 Seller Update ====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
-
+		
+		
+		System.out.println("\n === TESTE 6 Seller Delete ====");
+		System.out.println("Type an id for delete test:");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
 	}
 
 }
